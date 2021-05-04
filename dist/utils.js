3f457cbc8ee9528cc39773b850f79d6f1acff3b9
dino16m <anselem16m@gmail.com>
2021-05-04 23:09:46 +0100

    change typescript to dependency

--- dist/utils.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    daysBetweenDates(start, end) {
-        const diffInMillisecs = (end.getTime() - start.getTime());
-        return diffInMillisecs / (1000 * 3600 * 24);
-    }
-};
-//# sourceMappingURL=utils.js.map
